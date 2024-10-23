faa097f2189d4c6341e412961a06c402a7dfd7e2
Solomon Marita <officialmokua@gmail.com>
2024-10-23 17:46:22 +0300

    Added vs code editor ruler settings to wrap lines at 79 characters

--- Python/codewithmosh/python_projects_for_beginners/dice_rolling_game.py
+++ Python/codewithmosh/python_projects_for_beginners/dice_rolling_game.py
@@ -1,9 +1,9 @@
 """
 ## Dice Rolling Game
 
-Write a program that simulates rolling a pair of dice. Each time the program runs, it
-should randomly generate two numbers between 1 and 6 (inclusive), representing
-the result of each die. The program should then display the results and ask if the
+A program that simulates rolling a pair of dice. Each time the program runs, it
+randomly generate two numbers between 1 and 6 (inclusive), representing the
+result of each die. The program should then display the results and ask if the
 user would like to roll again.
 """
 import random
