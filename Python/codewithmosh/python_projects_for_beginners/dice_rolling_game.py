b945a61ba678d28b6d98d50536f776648f3cb712
Solomon Marita <officialmokua@gmail.com>
2024-10-23 17:24:44 +0300

    Completed dice rolling game

---
+++ Python/codewithmosh/python_projects_for_beginners/dice_rolling_game.py
@@ -0,0 +1,21 @@
+"""
+## Dice Rolling Game
+
+Write a program that simulates rolling a pair of dice. Each time the program runs, it
+should randomly generate two numbers between 1 and 6 (inclusive), representing
+the result of each die. The program should then display the results and ask if the
+user would like to roll again.
+"""
+import random
+
+while True:
+    choice: str = input('Roll the dice? (y/n): ')
+    if choice.casefold() == 'y':
+        die_1: int = random.randint(1, 6)
+        die_2: int = random.randint(1, 6)
+        print(f'({die_1}, {die_2})')
+    elif choice.casefold() == 'n':
+        print('Thanks for playing!')
+        break
+    else:
+        print('Invalid Choice!')
