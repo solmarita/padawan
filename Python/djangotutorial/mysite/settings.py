fbfcab23a94c52a068295464a461f85c48f129bd
Solomon Marita <officialmokua@gmail.com>
2025-08-13 21:57:03 +0300

    Set up the Django project and created the polls app

--- Python/djangotutorial/mysite/settings.py
+++ Python/djangotutorial/mysite/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)#w!^08@%w0u74zv4n3&u)8a%6-(!-*k#p3g2l3#jmj76s#8hj'
+
+load_dotenv()  # Load environment variables from .env file
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+
+if not SECRET_KEY:
+    raise ValueError("DJANGO_SECRET_KEY not set in environment")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
